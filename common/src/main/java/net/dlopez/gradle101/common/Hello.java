b1d6e7813d47136dc63c0b2fa622a124ac8d47a2
David Lopez <espencer@gmail.com>
2021-12-15 16:48:28 +0100

    Add intentionally CVE-2021-44228 (#1)

--- common/src/main/java/net/dlopez/gradle101/common/Hello.java
+++ common/src/main/java/net/dlopez/gradle101/common/Hello.java
@@ -1,7 +1,14 @@
 package net.dlopez.gradle101.common;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class Hello {
+    protected static final Logger logger = LogManager.getLogger();
+
     public void SayHello() {
+        logger.log(Level.ERROR, "Hey");
         System.out.println("Hello World");
     }
 }
